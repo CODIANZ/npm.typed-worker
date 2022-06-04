63484cbf8fecf76898223f1a91479dc6487e973f
Terukazu Inoue <terukazu.inoue@codianz.com>
2022-06-05 00:53:40 +0900

    added debug settings

--- test/vue.config.js
+++ test/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    devtool: process.env.NODE_ENV === "production" ? false : "source-map"
+  }
 });
